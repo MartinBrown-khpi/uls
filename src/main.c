4f58bfc95870d1bf4b8f1beda7a0f3c45ff098b9
Mykola Nosov <mnosov@c1r6p15.khpi.ucode-connect.study>
2021-11-12 17:43:11 +0200

    delete uls from list of items

--- src/main.c
+++ src/main.c
@@ -44,15 +44,22 @@ int main(int argc, char const *argv[]) {
     closedir(dp);
     
     struct dirent **dirp = (struct dirent **)malloc(sizeof(struct dirent *) * size_dirp);
-
     dp = opendir(".");
     for (int i = 0; i < size_dirp; i++) {
         dirp[i] = readdir(dp);
+        if (mx_strcmp(dirp[i]->d_name, "uls") == 0) {
+            i--;
+            size_dirp--;
+        }
         if (dirp[i] == NULL) {
             mx_printerr("cant read\n");
             exit(1);
         }
     }
+    // Vozmozhno mesto ute4ki ili oshibki
+    // Elsi naebnetsa, smotret suda 
+    dirp = mx_realloc(dirp, size_dirp);
+
 
     closedir(dp);
 
