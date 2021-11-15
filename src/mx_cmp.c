95899ef7c658d2c006bc45ce0caf3c35f3f8cec4
Andrii Yevtushenko <ayevtushen@c1r6p15.khpi.ucode-connect.study>
2021-11-15 18:27:27 +0200

    flags

--- src/mx_cmp.c
+++ src/mx_cmp.c
@@ -1,13 +1,7 @@
 #include "uls.h"
 
-#include <stdio.h>
 //return true if 1 > 2
 bool mx_default_cmp(long_data_t *first, long_data_t *sec) {
-    //printf("default cmp\n");
-    if (!first || !sec) {
-        mx_printerr("NULL pointer in default cmp");
-        return false;
-    }
 
     if (mx_strcmp(first->f_namefile, ".") == 0) {
         return false;
@@ -27,11 +21,7 @@ bool mx_default_cmp(long_data_t *first, long_data_t *sec) {
 //return true if 1 > 2
 //flag -S
 bool mx_size_cmp(long_data_t *first, long_data_t *sec) {
-    //printf("size cmp\n");
-    if (!first || !sec) {
-        mx_printerr("NULL pointer in size cmp");
-        return false;
-    }
+
     if (first->f_size < sec->f_size) {
         return true;
     }
@@ -41,11 +31,6 @@ bool mx_size_cmp(long_data_t *first, long_data_t *sec) {
 //return true if 1 > 2
 // flag -t
 bool mx_time_modif_cmp(long_data_t *first, long_data_t *sec) {
-    //printf("mx_time_modif_cmp cmp\n");
-    if (!first || !sec) {
-        mx_printerr("NULL pointer in time_modif_cmp");
-        return false;
-    }
 
     if (first->f_time_modification->tv_sec < sec->f_time_modification->tv_sec) {
         return true;
@@ -60,14 +45,15 @@ bool mx_time_modif_cmp(long_data_t *first, long_data_t *sec) {
 
 //return true if 2 > 1 
 bool mx_time_access_cmp(long_data_t *first, long_data_t *sec) {
-    //printf("mx_time_access_cmp cmp\n");
-    if (!first || !sec) {
-        mx_printerr("NULL pointer in time_modif_cmp");
-        return false;
-    }
+
 
     if (first->f_time_last_acces->tv_sec < sec->f_time_last_acces->tv_sec) {
         return true;
+    }
+    else if (first->f_time_last_acces->tv_sec == sec->f_time_last_acces->tv_sec) {
+        if (mx_strcmp(first->f_namefile, sec->f_namefile) > 0) {
+            return true;
+        }
     }
     return false;
 }
@@ -75,16 +61,11 @@ bool mx_time_access_cmp(long_data_t *first, long_data_t *sec) {
 // last  changed
 // flag -c  
 bool mx_time_status_cmp(long_data_t *first, long_data_t *sec) {
-printf("mx_time_status_cmp\n");
-    if (!first || !sec) {
-        mx_printerr("NULL pointer in time_modif_cmp");
-        return false;
-    }
 
     if (first->f_time_last_status->tv_sec < sec->f_time_last_status->tv_sec) {
         return true;
     }
-    else if (first->f_time_last_status->tv_sec > sec->f_time_last_status->tv_sec) {
+    else if (first->f_time_last_status->tv_sec == sec->f_time_last_status->tv_sec) {
         if (mx_strcmp(first->f_namefile, sec->f_namefile) > 0) {
             return true;
         }
